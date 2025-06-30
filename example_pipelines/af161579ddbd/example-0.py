93c472385aef37faeb28fd8482caf2c4c87ec218
TheodorWienert <theo@wienert-hamburg.de>
2025-06-30 15:08:53 +0200

    Init

---
+++ example_pipelines/af161579ddbd/example-0.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+# Setting up paths
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from utils import get_project_root
+
+# Getting the project root
+project_root = get_project_root()
+
+# Getting the raw data file
+raw_data_file = os.path.join(
+    project_root, "datasets", "c99d9bc33649", "c99d9bc33649_b.csv"
+)
+data = pd.read_csv(raw_data_file)
+
+# Splitting the raw data into training and test sets
+X = data.drop("Diabetes_binary", axis=1)
+y = data["Diabetes_binary"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Training the model
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+# Evaluating the model
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
